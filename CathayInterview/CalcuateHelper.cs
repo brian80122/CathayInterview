50ff4e10c99b6cff0af624faacdcbd15ede5f710
brian80122 <brian80122@gmail.com>
2023-10-26 20:59:46 +0800

    init commit

---
+++ CathayInterview/CalcuateHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace CathayInterview
+{
+    public static class CalculateHelper
+    {
+
+        public static string[] Calculate(decimal rate, string[] input)
+        {
+            var sortedAmounts = input
+                .Select(x => ((x.Contains("-") ? 0 : decimal.Parse(x)) * rate).ToString(CultureInfo.InvariantCulture))
+                .OrderByDescending(x => x)
+                .ToArray();
+
+            return sortedAmounts;
+        }
+    }
+}
---
+++ CathayInterview/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CathayInterview;
+
+string[] amountList = { "1.2", "1.4", "0.2", "-", "-0.005" };
+decimal rate = 0.33m;
+
+var result = CalculateHelper.Calculate(rate, amountList);
+
+Console.WriteLine(string.Join($@", ", result));
+
+Console.ReadKey();
+
