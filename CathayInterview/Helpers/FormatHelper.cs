9b91d740031267139e2a7d2c1aa0565263a8c2bc
Brian Liang <brian.liang@gobuid.com>
2023-10-27 09:20:35 +0800

    add formatHelper and test

---
+++ CathayInterview/Helpers/FormatHelper.cs
@@ -0,0 +1,24 @@
+﻿namespace CathayInterview.Helpers
+{
+    public static class FormatHelper
+    {
+        public static string FormatInput<T>(T input)
+        {
+            if (input == null)
+                throw new ArgumentException($@"Input can not be null");
+
+            if (typeof(T) == typeof(int) || typeof(T) == typeof(string))
+            {
+                return input.ToString()!;
+            }
+
+            if (typeof(T) == typeof(DateTime))
+            {
+                var date = (DateTime)(object)input;
+                return date.ToString("yyyy/MM/dd");
+            }
+
+            throw new ArgumentException("Unsupported input type");
+        }
+    }
+}
---
+++ CathayInterviewTest/FormatHelperTests.cs
@@ -0,0 +1,71 @@
+﻿using CathayInterview.Helpers;
+using NUnit.Framework;
+using System;
+
+namespace CathayInterviewTest
+{
+    public class FormatHelperTests
+    {
+        [Theory]
+        [TestCase(123, "123")]
+        [TestCase(456, "456")]
+        public void FormatInput_IntInput_ReturnsExpectedResult(int input, string expectedResult)
+        {
+            // Act
+            string result = FormatHelper.FormatInput(input);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Theory]
+        [TestCase("hello", "hello")]
+        [TestCase("world", "world")]
+        public void FormatInput_StringInput_ReturnsExpectedResult(string input, string expectedResult)
+        {
+            // Act
+            string result = FormatHelper.FormatInput(input);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Theory]
+        [TestCaseSource(nameof(_getDateTimeCases))]
+        public void FormatInput_DateTimeInput_ReturnsExpectedResult(DateTime input, string expectedResult)
+        {
+            // Act
+            string result = FormatHelper.FormatInput(input);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        private static object[] _getDateTimeCases =
+        {
+            new object[] { new DateTime(2022, 6, 20,12,05,33), "2022/06/20" },
+            new object[] { new DateTime(2021, 10, 5,22,15,19), "2021/10/05" }
+        };
+
+
+        [Test]
+        public void FormatInput_NullInput_ThrowsArgumentException()
+        {
+            // Arrange
+            object? input = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => FormatHelper.FormatInput(input), "Input can not be null");
+        }
+
+        [Test]
+        public void FormatInput_UnsupportedTypeInput_ThrowsArgumentException()
+        {
+            // Arrange
+            object input = new object(); // an unsupported type
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => FormatHelper.FormatInput(input), "Unsupported input type");
+        }
+    }
+}
